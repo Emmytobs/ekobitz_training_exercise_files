4a38ba131f5469c6c2d0ae9526d750e745dfc033
Emmytobs <eotobo11@gmail.com>
2020-10-04 23:11:43 +0100

    Finished Part 1 of the solutions to the Events Exercises

---
+++ 07_Events_Exercises/script.js
@@ -0,0 +1,20 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const heading = document.querySelector("h1");
+    heading.innerText = "Hello World!"
+    
+    const colouredBoxes = document.querySelectorAll('section div');
+    const selectedColor = document.querySelector('span.selected');
+
+    colouredBoxes.forEach(box => {
+        box.addEventListener('mouseover', (e) => {
+            let classNameColor = e.target.className
+            selectedColor.innerText = classNameColor
+        })
+    })
+    
+    const newDiv = document.createElement('div');
+    const colouredBoxesContainer = document.querySelector('section');
+    newDiv.className = 'purple';
+    colouredBoxesContainer.appendChild(newDiv);
+    
+})
---
+++ 07_Events_Exercises/solution.md
@@ -0,0 +1,4 @@
+This file contains solutions to the exercises in the *[Events and Local Storage](https://snoseeds.github.io/inf-paces-school/courses/intermediate-javascript/javascript-dom-events-exercises.html)* course
+
+# Part 1
+Given the following HTML, create a script.js file to complete the first two parts.
