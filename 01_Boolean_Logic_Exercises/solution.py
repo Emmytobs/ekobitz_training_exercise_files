15c1b91d55cf29b529dcd47166f4c770b6071df7
Emmytobs <eotobo11@gmail.com>
2020-12-05 14:19:38 +0100

    Added solution file for the exercise

---
+++ 01_Boolean_Logic_Exercises/solution.py
@@ -0,0 +1,46 @@
+#1
+first = "Hello World"
+#2
+# This is a comment
+#3
+print("I am a Computer!")
+#4
+if 1 < 2 and 4 > 2:
+    print("Math is fun.")
+#5
+nope = Null
+#6
+True and False
+#7
+str_length = len("What's my length?")
+#8
+str_uppercase = "i am shouting".upper()
+#9
+num = int("1000");
+# 10
+string = "real"
+num = 4
+print(str(num) + string)
+#11
+print(3 * "cool")
+# 12
+print(1/0)
+# 13
+print(type([]))
+# 14
+name = input("What's your name")
+# 15
+number = input("Put in any number")
+number = int(number)
+if number < 0:
+    print("That number is less than 0!")
+elif number > 0:
+    print("That number is greater than 0!")
+else:
+    print("You picked 0!")
+# 16
+index_of_letter = "apple".find("l")
+# 17
+index_of_letter = "xylophone".find("y")
+# 18
+is_lower_case = `"my_string".islower()`
